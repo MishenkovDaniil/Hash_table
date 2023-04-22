ba88b9c6756edfdcbe881b968a05af9b1ee1365e
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-04-23 00:49:03 +0300

    add parsing text in array of strings

---
+++ parser/parser.cpp
@@ -0,0 +1,141 @@
+
+#include "parser.h"
+
+Parsed_text *parce_text (const char *filename)
+{
+    assert (filename);
+
+    char *text = read_text (filename);
+    assert (text);
+
+    Parsed_text *parsed_text = (Parsed_text *)calloc (1, sizeof (Parsed_text));
+
+    parsed_text->arr = (char **)calloc (START_SIZE, sizeof (char *));
+    assert (parsed_text->arr);
+    parsed_text->capacity = START_SIZE;
+    
+    int idx = 0;
+
+    char *cur_smbl_ptr = text;
+
+    char *buf = (char *)calloc (MAX_STR_SIZE, sizeof(char));
+    
+    while (*(cur_smbl_ptr))
+    {
+        char cur_smbl = *cur_smbl_ptr;
+        size_t word_size = 0;
+
+        if (isspace (cur_smbl) || ispunct (cur_smbl) || isdigit (cur_smbl))
+        {
+            ++cur_smbl_ptr;
+            continue;
+        }
+
+        sscanf (cur_smbl_ptr, "%[a-zA-z_1-9]%n", buf, &word_size);
+        buf[word_size] = '\0';
+        
+        if (!word_size)
+        {
+            ++cur_smbl_ptr;
+            continue;
+        }
+
+        parsed_text->arr[idx] = (char *)calloc (word_size + 1, sizeof (char));
+        sprintf (parsed_text->arr[idx], "%s", buf);
+        fprintf (stderr, "[%s]\n", parsed_text->arr[idx]);
+
+        cur_smbl_ptr += word_size;
+        ++idx;
+
+        if (idx == parsed_text->capacity)
+        {
+            realloc_text (parsed_text); //check on -1
+        }
+    }
+
+    parsed_text->size = idx;
+
+    free (buf);
+    free (text);
+    
+    return parsed_text;
+}
+
+int realloc_text (Parsed_text *parsed_text)
+{
+    assert (parsed_text && parsed_text->arr);
+
+    parsed_text->arr = (char **)realloc ((void *)parsed_text->arr, sizeof(char *) * parsed_text->capacity * 2);
+    assert (parsed_text->arr);  ///return -1
+
+    parsed_text->capacity *= 2;
+
+    return 0;
+}
+
+void parced_text_dtor (Parsed_text *parsed_text)
+{
+    if (!(parsed_text && parsed_text->arr))
+        return;
+    
+    parced_arr_dtor (parsed_text->arr, parsed_text->size);
+
+    free (parsed_text->arr);
+    parsed_text->arr = nullptr;
+
+    free (parsed_text);
+    parsed_text = nullptr;
+}
+
+void parced_arr_dtor (char **arr, size_t size)
+{
+    int idx = 0;
+    while (idx < size)
+    {
+        if (arr[idx])
+        {
+            free (arr[idx]);
+            arr[idx] = nullptr;
+        }
+
+        ++idx;
+    }
+}
+
+
+char *read_text (const char *filename)
+{
+    assert (filename);
+
+    int file_descr = open (filename, O_RDONLY);
+    size_t filesize = get_file_size (filename);
+
+    char *buf = (char *)calloc (filesize + 1, sizeof (char));
+    assert (buf);
+
+    int status = read (file_descr, buf, filesize);
+    
+    if (status == -1)
+    {
+        perror ("read () error: ");
+        return nullptr;
+    }
+
+    printf ("read %d from %d\n", status, filesize);
+
+    return buf;
+}
+
+size_t get_file_size (const char *filename)
+{
+    struct stat buf = {};
+
+    if (stat (filename, &buf) == -1)
+    {
+        perror ("stat() failed.\n");
+
+        return 0;
+    }
+
+    return buf.st_size;
+}
---
+++ parser/parser.h
@@ -0,0 +1,35 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <assert.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+static const size_t START_SIZE = 100;
+static const size_t MAX_STR_SIZE = 1000;
+
+struct Parsed_text
+{
+    char **arr = nullptr;
+    size_t size = 0;
+    size_t capacity = 0;
+};
+
+/// public
+Parsed_text *parce_text (const char *filename);
+void parced_text_dtor (Parsed_text *parced_text);
+
+/// private 
+void parced_arr_dtor (char **arr, size_t size);
+int realloc_text (Parsed_text *parced_text);
+
+
+char *read_text (const char *filename);
+size_t get_file_size (const char *filename);
+
+#endif /* PARSER_H */
