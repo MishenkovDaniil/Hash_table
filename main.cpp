168c46272988b591cf3d114df5af61040c5b0cda
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-04-26 01:47:43 +0300

    add function filling hash mmaps with different hash functions with words from parsed text

--- main.cpp
+++ main.cpp
@@ -12,6 +12,11 @@
 #include "hash_functions/hash_functions.h"
 
 const char *TEXT_FILENAME = "texts/war_and_peace.txt";
+const char *DUMP_FILENAME = "hash_dump";
+const size_t HASH_FUNCTIONS_NUM = 6;
+
+void hash_comparison (Parsed_text *parsed_text, FILE *dump_file);
+
 
 int main ()
 {
@@ -19,21 +24,43 @@ int main ()
     fprintf (stderr, "size = %lu\n", parsed_text->size);
     fprintf (stderr, "size = %lu\n", parsed_text->capacity);
 
-    Hash *hash = Hash_ctor (233, hash_1, 100);
-    
-    FILE *dump_file = fopen ("hash_dump", "w");
+    FILE *dump_file = fopen (DUMP_FILENAME, "w");
     assert (dump_file);
     
-    for (size_t iter = 0; iter < parsed_text->size; ++iter)
-    {
-        Hash_insert (hash, parsed_text->arr[iter]);
-    }
-
-    hash_dump (hash, dump_file);
-
-    Hash_dtor (hash);
+    hash_comparison (parsed_text, dump_file);
 
     parced_text_dtor (parsed_text);
-    
+
     return 0;
+}
+
+void hash_comparison (Parsed_text *parsed_text, FILE *dump_file)
+{
+    assert (parsed_text && dump_file);
+    
+    Hash **hash_mmaps = (Hash **)calloc (HASH_FUNCTIONS_NUM, sizeof (Hash *));
+
+    hash_mmaps[0] = Hash_ctor (233, hash_ret_1, 100);
+    hash_mmaps[1] = Hash_ctor (233, hash_first_ch, 100);
+    hash_mmaps[2] = Hash_ctor (233, hash_strlen, 100);
+    hash_mmaps[3] = Hash_ctor (233, hash_ch_sum, 100);
+    hash_mmaps[4] = Hash_ctor (233, hash_rotr, 100);
+    hash_mmaps[5] = Hash_ctor (233, hash_rotl, 100);
+
+    for (size_t hash_func_num = 0; hash_func_num < HASH_FUNCTIONS_NUM; ++hash_func_num)
+    {
+        for (size_t iter = 0; iter < parsed_text->size; ++iter)
+        {
+            if (!(Hash_exists (hash_mmaps[hash_func_num], parsed_text->arr[iter])))
+            {
+                Hash_insert (hash_mmaps[hash_func_num], parsed_text->arr[iter]);
+            }
+        }
+
+        hash_dump (hash_mmaps[hash_func_num], dump_file);
+
+        Hash_dtor (hash_mmaps[hash_func_num]);
+    }
+
+    free (hash_mmaps);
 }
