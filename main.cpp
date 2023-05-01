3710a9ffb1f06996631b66b3f0e7eeab77a83c93
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-05-01 20:50:11 +0300

    add timetest of search with one hash_function and made dump in debug mode

--- main.cpp
+++ main.cpp
@@ -6,23 +6,38 @@
 #include <fcntl.h>
 #include <string.h>
 #include <ctype.h>
+#include <nmmintrin.h>
+
+// #define DEBUG
 
 #include "hash.h"
 #include "parser/parser.h"
 #include "hash_functions/hash_functions.h"
 
-const char *TEXT_FILENAME = "texts/war_and_peace.txt";
-const char *DUMP_FILENAME = "hash_dump";
-const size_t HASH_FUNCTIONS_NUM = 6;
+#define WORK_TIME_CHECK
 
-void hash_comparison (Parsed_text *parsed_text, FILE *dump_file);
+static const char *CSV_FILENAME   = "data/histograms/hash.csv";
+static const char *CSV_FILENAME_1 = "data/histograms/hash_1.csv";
+static const char *CSV_FILENAME_2 = "data/histograms/hash_2.csv";
+static const char *CSV_FILENAME_3 = "data/histograms/hash_3.csv";
+static const char *CSV_FILENAME_4 = "data/histograms/hash_4.csv";
+static const char *CSV_FILENAME_5 = "data/histograms/hash_5.csv";
+static const char *CSV_FILENAME_6 = "data/histograms/hash_6.csv";
+static const char *CSV_FILENAME_7 = "data/histograms/hash_7.csv";
+static const char *TEXT_FILENAME  = "data/texts/war_and_peace.txt";
+static const char *DUMP_FILENAME  = "data/dump_data/hash_dump";
+
+static const size_t HASH_FUNCTIONS_NUM = 7;
+static const size_t HASH_MMAP_CAPACITY = 499;
+static const size_t INIT_LIST_CAPACITY = 100;
+static const size_t SEARCH_ITER_NUM    = 5000000;
+
+void hash_comparison (Parsed_text *parsed_text, FILE *dump_file, const bool is_no_repeats = false);
 
 
 int main ()
-{
+{   
     Parsed_text *parsed_text = parce_text (TEXT_FILENAME);
-    fprintf (stderr, "size = %lu\n", parsed_text->size);
-    fprintf (stderr, "size = %lu\n", parsed_text->capacity);
 
     FILE *dump_file = fopen (DUMP_FILENAME, "w");
     assert (dump_file);
@@ -34,33 +49,58 @@ int main ()
     return 0;
 }
 
-void hash_comparison (Parsed_text *parsed_text, FILE *dump_file)
+void hash_comparison (Parsed_text *parsed_text, FILE *dump_file, const bool is_no_repeats)
 {
     assert (parsed_text && dump_file);
-    
+
     Hash **hash_mmaps = (Hash **)calloc (HASH_FUNCTIONS_NUM, sizeof (Hash *));
+    assert (hash_mmaps);
 
-    hash_mmaps[0] = Hash_ctor (233, hash_ret_1, 100);
-    hash_mmaps[1] = Hash_ctor (233, hash_first_ch, 100);
-    hash_mmaps[2] = Hash_ctor (233, hash_strlen, 100);
-    hash_mmaps[3] = Hash_ctor (233, hash_ch_sum, 100);
-    hash_mmaps[4] = Hash_ctor (233, hash_rotr, 100);
-    hash_mmaps[5] = Hash_ctor (233, hash_rotl, 100);
+    FILE *csv_file = fopen (CSV_FILENAME, "w");
+    assert (csv_file);
 
-    for (size_t hash_func_num = 0; hash_func_num < HASH_FUNCTIONS_NUM; ++hash_func_num)
+    hash_mmaps[0] = Hash_ctor (HASH_MMAP_CAPACITY, hash_ret_1, INIT_LIST_CAPACITY);
+    hash_mmaps[1] = Hash_ctor (HASH_MMAP_CAPACITY, hash_first_ch, INIT_LIST_CAPACITY);
+    hash_mmaps[2] = Hash_ctor (HASH_MMAP_CAPACITY, hash_strlen, INIT_LIST_CAPACITY);
+    hash_mmaps[3] = Hash_ctor (HASH_MMAP_CAPACITY, hash_ch_sum, INIT_LIST_CAPACITY);
+    hash_mmaps[4] = Hash_ctor (HASH_MMAP_CAPACITY, hash_rotr, INIT_LIST_CAPACITY);
+    hash_mmaps[5] = Hash_ctor (HASH_MMAP_CAPACITY, hash_rotl, INIT_LIST_CAPACITY);
+    hash_mmaps[6] = Hash_ctor (HASH_MMAP_CAPACITY, hash_crc64_opt, INIT_LIST_CAPACITY);
+
+    for (size_t hash_func_num = 0; hash_func_num < 7; ++hash_func_num)
     {
         for (size_t iter = 0; iter < parsed_text->size; ++iter)
         {
-            if (!(Hash_exists (hash_mmaps[hash_func_num], parsed_text->arr[iter])))
+            if (is_no_repeats || !(Hash_exists (hash_mmaps[hash_func_num], parsed_text->arr[iter])))
             {
                 Hash_insert (hash_mmaps[hash_func_num], parsed_text->arr[iter]);
             }
         }
-
-        hash_dump (hash_mmaps[hash_func_num], dump_file);
-
-        Hash_dtor (hash_mmaps[hash_func_num]);
+        
+        #ifdef DEBUG
+        hash_dump (hash_mmaps[hash_func_num], dump_file, csv_file);
+        #endif 
     }
 
+    #ifdef WORK_TIME_CHECK
+    for (int i = 0; i < SEARCH_ITER_NUM; ++i)
+    {
+        hash_find (hash_mmaps[6], parsed_text->arr[i % parsed_text->size]);
+    }
+    #endif 
+
+    for (size_t hash_func_num = 0; hash_func_num < 7; ++hash_func_num)
+    {
+        Hash_dtor (hash_mmaps[hash_func_num]);
+    }
+    
+    for (int i = 0; i < HASH_MMAP_CAPACITY; ++i)
+    {
+        fprintf (csv_file, "%d,", i);
+    }
+
+    fprintf (csv_file, "\n");
+
+    fclose (csv_file);
     free (hash_mmaps);
 }
