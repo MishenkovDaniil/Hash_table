41b6e2e73f691844ed8541efc0fa7fb77557bc31
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-05-01 20:51:20 +0300

    add function headers

--- hash.h
+++ hash.h
@@ -19,9 +19,10 @@ Hash *Hash_ctor (size_t size, size_t (*hash_func)(char *string), size_t init_lis
 void Hash_insert (Hash *hash, char *string);
 void Hash_dtor (Hash *hash);
 void clear_hash_arr (List **arr, size_t size);
-void hash_dump (Hash *hash, FILE *dump_file);
+void hash_dump (Hash *hash, FILE *dump_file, FILE *csv_file);
 void dump_hash (Hash *hash, FILE *dump_file);
-void dump_hash_lists (List **arr, size_t arr_size, FILE *dump_file);
+void dump_hash_lists (List **arr, size_t arr_size, FILE *dump_file, FILE *csv_file);
 bool Hash_exists (Hash *hash, char *string);
+List_elem *hash_find (Hash *hash, char *string);
 
 #endif /* HASH_H */
