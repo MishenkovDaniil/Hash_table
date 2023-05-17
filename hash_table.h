9df59ed3c6d76935dd54e5faae0ba40834a08816
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-05-17 21:41:28 +0300

    renamed defines, delete unused function declaratiuon, changed it for ctor

--- hash_table.h
+++ hash_table.h
@@ -1,5 +1,5 @@
-#ifndef HASH_H
-#define HASH_H
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
 
 #include <assert.h>
 #include <stdio.h>
@@ -11,7 +11,7 @@ struct Hash_table
 {
     size_t size = 0;
     List *arr = nullptr;
-    size_t (*hash_func)(char *string) = nullptr;
+    size_t (*hash_func)(char *) = nullptr;
     size_t init_list_capacity = 0;
 };
 
@@ -19,15 +19,14 @@ struct Hash_table
 // 1.изначально аллоцируем меньше памяти (только указатели)
 // 2.проверка указатель на nullptr вместо инициализации всех структур и проверки их размера
 
-Hash_table *hash_table_ctor (size_t size, size_t (*hash_func)(char *string), size_t init_list_capacity);
+void hash_table_ctor (Hash_table *hash_table, size_t size, size_t (*hash_func)(char *), size_t init_list_capacity);
 void hash_table_insert (Hash_table *hash, char *string);
 bool hash_table_exists (Hash_table *hash, char *string);
 void hash_table_dump (Hash_table *hash, FILE *dump_file, FILE *csv_file);
 void hash_table_dtor (Hash_table *hash);
 List_elem *hash_table_find (Hash_table *hash, char *string);
 
-void clear_hash_table_arr (List *arr, size_t size);
 void dump_hash_table (Hash_table *hash, FILE *dump_file);
 void dump_hash_table_lists (List *arr, size_t arr_size, FILE *dump_file, FILE *csv_file);
 
-#endif /* HASH_H */
+#endif /* HASH_TABLE_H */
