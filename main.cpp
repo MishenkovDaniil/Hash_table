f09c5915a7a1239a8aa686b3ef69d2f59d720e09
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-05-17 21:43:47 +0300

    made load functions filling hash table array and hash table with text, made founded value of hash_find() volatile, made filling hash_mmaps through the array of hash_function pointers, changed code according to changes in hash_table.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,10 @@
 #include <ctype.h>
 #include <nmmintrin.h>
 #include <time.h>
+
 // #define HASH_DEBUG
 
-#include "hash.h"
+#include "hash_table.h"
 #include "parser/parser.h"
 #include "hash_functions/hash_functions.h"
 #include "root_mean_square/rms.h"
@@ -18,27 +19,32 @@
 #define WORK_TIME_CHECK
 
 static const char *CSV_FILENAME   = "data/histograms/hash.csv";
-static const char *CSV_FILENAME_1 = "data/histograms/hash_1.csv";
-static const char *CSV_FILENAME_2 = "data/histograms/hash_2.csv";
-static const char *CSV_FILENAME_3 = "data/histograms/hash_3.csv";
-static const char *CSV_FILENAME_4 = "data/histograms/hash_4.csv";
-static const char *CSV_FILENAME_5 = "data/histograms/hash_5.csv";
-static const char *CSV_FILENAME_6 = "data/histograms/hash_6.csv";
-static const char *CSV_FILENAME_7 = "data/histograms/hash_7.csv";
 static const char *TEXT_FILENAME  = "data/texts/parsed_text.txt";
 static const char *DUMP_FILENAME  = "data/dump_data/hash_dump";
 
-static const size_t HASH_FUNCTIONS_NUM = 7;
+static const size_t MAX_HASH_FUNCTIONS_NUM = 10;
 static const size_t HASH_MMAP_CAPACITY = 4999;
 static const size_t INIT_LIST_CAPACITY = 100;
 static const size_t SEARCH_ITER_NUM    = 5000000;
 static const size_t TIMETEST_HASH_IDX  = 6;
 static const size_t CALC_NUM = 20;
 
-static const int IS_SORTED = 0;
-void sort_text (Parsed_text *parsed_text, FILE *parsed_text_file);
+//массив указателей на хэш-функции с нулем в конце
 
-void hash_comparison (Parsed_text *parsed_text, FILE *dump_file, const bool is_no_repeats = false);
+static const int IS_SORTED = 0;
+static const bool IS_NO_REPEATS = true;
+
+size_t (*hash_func[])(char *) = {hash_ret_1,  hash_first_ch, 
+                                 hash_strlen, hash_ch_sum, 
+                                 hash_rotr,   hash_rotl,
+                                 hash_crc64,  nullptr};
+
+void sort_text (Parsed_text *parsed_text, FILE *parsed_text_file);
+void hash_comparison (Parsed_text *parsed_text, FILE *dump_file, size_t (**hash_func)(char *), const bool is_no_repeats = false);
+void load_hash_table (Hash_table *hash_table, Parsed_text *parsed_text, bool is_no_repeats);
+void hash_table_work_time (Hash_table *hash_table, Parsed_text *parsed_text, const size_t calc_num, const size_t search_iter_num);
+void print_elem_idxs (FILE *file, size_t elem_num);
+size_t load_hash_mmaps (Hash_table *hash_maps, size_t (**hash_func)(char *));
 
 int main ()
 {   
@@ -46,6 +52,7 @@ int main ()
 
     FILE *dump_file = fopen (DUMP_FILENAME, "w");
     assert (dump_file);
+
     // // if (!IS_SORTED)
     // // {
     // //     FILE *parsed_text_file = fopen ("data/texts/parsed_text.txt", "w");
@@ -54,83 +61,124 @@ int main ()
     // //     fclose (parsed_text_file);
     // // }
 
-    hash_comparison (parsed_text, dump_file);
+    hash_comparison (parsed_text, dump_file, hash_func, IS_NO_REPEATS);
 
     parced_text_dtor (parsed_text);
 
     return 0;
 }
 
-void hash_comparison (Parsed_text *parsed_text, FILE *dump_file, const bool is_no_repeats)
+void hash_comparison (Parsed_text *parsed_text, FILE *dump_file, size_t (**hash_func)(char *), const bool is_no_repeats)
 {
     assert (parsed_text && dump_file);
     
-    Hash **hash_mmaps = (Hash **)calloc (HASH_FUNCTIONS_NUM, sizeof (Hash *));
-    assert (hash_mmaps);
+    Hash_table *hash_maps = (Hash_table *)calloc (MAX_HASH_FUNCTIONS_NUM, sizeof (Hash_table));
+    assert (hash_maps);
 
     FILE *csv_file = fopen (CSV_FILENAME, "w");
     assert (csv_file);
 
-    hash_mmaps[0]                   = Hash_ctor (HASH_MMAP_CAPACITY, hash_ret_1,     INIT_LIST_CAPACITY);
-    hash_mmaps[1]                   = Hash_ctor (HASH_MMAP_CAPACITY, hash_first_ch,  INIT_LIST_CAPACITY);
-    hash_mmaps[2]                   = Hash_ctor (HASH_MMAP_CAPACITY, hash_strlen,    INIT_LIST_CAPACITY);
-    hash_mmaps[3]                   = Hash_ctor (HASH_MMAP_CAPACITY, hash_ch_sum,    INIT_LIST_CAPACITY);
-    hash_mmaps[4]                   = Hash_ctor (HASH_MMAP_CAPACITY, hash_rotr,      INIT_LIST_CAPACITY);
-    hash_mmaps[5]                   = Hash_ctor (HASH_MMAP_CAPACITY, hash_rotl,      INIT_LIST_CAPACITY);
-    hash_mmaps[TIMETEST_HASH_IDX]   = Hash_ctor (HASH_MMAP_CAPACITY, hash_crc64_opt,     INIT_LIST_CAPACITY);
+    /// функция загрузки хэш-таблицы +
+    const size_t hash_func_num = load_hash_mmaps (hash_maps, hash_func);
     
-    for (size_t hash_func_num = 0; hash_func_num < HASH_FUNCTIONS_NUM; ++hash_func_num)
+    for (size_t cur_hash_func_num = 0; cur_hash_func_num < hash_func_num; ++cur_hash_func_num)
     {
-        for (size_t iter = 0; iter < parsed_text->size; ++iter)
-        {
-            // if (is_no_repeats || !(Hash_exists (hash_mmaps[hash_func_num], parsed_text->arr[iter])))
-            // {
-                Hash_insert (hash_mmaps[hash_func_num], parsed_text->arr[iter].word);
-            // }
-        }
+        load_hash_table (hash_maps + cur_hash_func_num, parsed_text, is_no_repeats);
         
         #ifdef HASH_DEBUG
-        hash_dump (hash_mmaps[hash_func_num], dump_file, csv_file);
+        hash_table_dump (hash_maps[cur_hash_func_num], dump_file, csv_file);
         #endif 
     }
 
     #ifdef WORK_TIME_CHECK
-    double time_ms = 0;
-    double values[CALC_NUM] = {0};
+    hash_table_work_time (hash_maps + TIMETEST_HASH_IDX, parsed_text, CALC_NUM, SEARCH_ITER_NUM);
+    #endif 
 
-    for (size_t calc_num = 0; calc_num < CALC_NUM; ++calc_num)
+    for (size_t cur_hash_func_num = 0; cur_hash_func_num < hash_func_num; ++cur_hash_func_num)
+    {
+        hash_table_dtor (hash_maps + cur_hash_func_num);
+    }
+    
+    print_elem_idxs (csv_file, HASH_MMAP_CAPACITY);
+    fprintf (csv_file, "\n");
+
+    fclose (csv_file);
+    free (hash_maps);
+}
+
+size_t load_hash_mmaps (Hash_table *hash_maps, size_t (**hash_func)(char *))
+{
+    assert (hash_maps && hash_func);
+
+    size_t hash_func_num = 0;
+
+    while (*hash_func)
+    {
+        hash_table_ctor (hash_maps + hash_func_num, HASH_MMAP_CAPACITY, *hash_func, INIT_LIST_CAPACITY);
+        hash_func++;
+        hash_func_num++;
+    }
+
+    return hash_func_num;
+}
+
+void load_hash_table (Hash_table *hash_table, Parsed_text *parsed_text, bool is_no_repeats)
+{
+    assert (hash_table && parsed_text);
+
+    for (size_t iter = 0; iter < parsed_text->size; ++iter)
+    {
+        if (is_no_repeats || !(hash_table_exists (hash_table, parsed_text->arr[iter].word)))
+        {
+            hash_table_insert (hash_table, parsed_text->arr[iter].word);
+        }
+    }
+}
+
+void hash_table_work_time (Hash_table *hash_table, Parsed_text *parsed_text, const size_t calc_num, const size_t search_iter_num)
+{
+    assert (hash_table && parsed_text);
+
+    double time_ms = 0;
+    double *values = (double *)calloc (calc_num, sizeof (double));
+
+    for (size_t cur_calc_num = 0; cur_calc_num < calc_num; ++cur_calc_num)
     {   
         clock_t t1 = clock();
-
-        for (int iter = 0; iter < SEARCH_ITER_NUM; ++iter)
+        for (int iter = 0; iter < search_iter_num; ++iter)
         {
-            hash_find (hash_mmaps[TIMETEST_HASH_IDX], parsed_text->arr[iter % parsed_text->size].word);
+///присваивать valotile +
+            volatile List_elem *founded = hash_table_find (hash_table, parsed_text->arr[iter % parsed_text->size].word);
+            // if (founded)
+            // {
+            //     if (strcmp (((List_elem *)founded)->data, parsed_text->arr[iter % parsed_text->size].word))
+            //     {
+            //         printf ("%s %s\n", ((List_elem *)founded)->data, parsed_text->arr[iter % parsed_text->size].word);
+            //         for (;;)
+            //             0;
+            //     }
+            //     printf ("yes");
+            // }
         }
 
         clock_t t2 = clock ();
 
-        values[calc_num]  = (double) (1000 * (double)(t2 - t1)) / CLOCKS_PER_SEC;
-        time_ms   += values[calc_num];
+        values[cur_calc_num]  = (double) (1000 * (double)(t2 - t1)) / CLOCKS_PER_SEC;
+        time_ms   += values[cur_calc_num];
     }
 
-    printf ("time = %lf\n", time_ms / CALC_NUM);
-    printf ("rsq = %lf\n", rms (values, CALC_NUM, time_ms / CALC_NUM));
+    printf ("time = %lf\n", time_ms / calc_num);
+    printf ("rms = %lf\n", rms (values, calc_num, time_ms / calc_num));
 
-    #endif 
+    free (values);
+}
 
+void print_elem_idxs (FILE *file, size_t elem_num)
+{
+    assert (file);
 
-    for (size_t hash_func_num = 0; hash_func_num < HASH_FUNCTIONS_NUM; ++hash_func_num)
-    {
-        Hash_dtor (hash_mmaps[hash_func_num]);
-    }
-    
     for (int idx = 0; idx < HASH_MMAP_CAPACITY; ++idx)
     {
-        fprintf (csv_file, "%d,", idx);
+        fprintf (file, "%d,", idx);
     }
-
-    fprintf (csv_file, "\n");
-
-    fclose (csv_file);
-    free (hash_mmaps);
 }
