6c93250adedfabcd88852ae66e573a27fe9fae1b
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-05-19 18:05:48 +0300

    made char *  parameter const in function declarations

--- hash_functions/hash_functions.h
+++ hash_functions/hash_functions.h
@@ -5,15 +5,15 @@
 #include <assert.h>
 #include <string.h>
 
-size_t hash_ret_1 (char *string);
-size_t hash_first_ch (char *string);
-size_t hash_strlen (char *string);
-size_t hash_ch_sum (char *string);
-size_t hash_rotr (char *string);
-size_t hash_rotl (char *string);
-size_t hash_crc64 (char *string);
-size_t hash_crc64_opt (char string[32]);
-size_t hash_crc64_char32 (char string[32]);
+size_t hash_ret_1 (const char *string);
+size_t hash_first_ch (const char *string);
+size_t hash_strlen (const char *string);
+size_t hash_ch_sum (const char *string);
+size_t hash_rotr (const char *string);
+size_t hash_rotl (const char *string);
+size_t hash_crc64 (const char *string);
+size_t hash_crc64_opt (const char string[32]);
+size_t hash_crc64_char32 (const char string[32]);
 
 size_t rotate_right (size_t val);
 size_t rotate_left (size_t val);
