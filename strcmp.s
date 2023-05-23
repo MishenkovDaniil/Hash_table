4d2fd88a441cb56b6dd7bacea570c8ebb605c843
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-05-23 04:03:23 +0300

    rewrote my strcmp implemenation for char[32]

--- strcmp.s
+++ strcmp.s
@@ -8,87 +8,30 @@ extern printf
 extern strlen 
 
 ;=============================================
-;int strcmp (const char *str1, const char *str2)                               
+;long strcmp (const char *str1, const char *str2)                               
 ;--------------------------------------------
 ;Entry: rdi = addr: str1, rsi = addr: str2 
-;Exit: eax = result
-;Destroys: rcx, rdi, rsi 
+;Exit: rax = result
+;Destroys: rdi, rsi 
 ;--------------------------------------------
 m_strcmp: 
-            ; pop qword [ret_addr]
-
-            mov ecx, 8
-            
-            mov eax, dword [rsi]
-            cmp eax, dword [rdi]
+            mov rax, qword [rsi]
+            sub rax, qword [rdi]
             jne .end 
 
-            add rsi, 4
-            add rdi, 4
-
-            mov eax, dword [rsi]
-            cmp eax, dword [rdi]
+            mov rax, qword [rsi + 8]
+            sub rax, qword [rdi + 8]
             jne .end 
 
-            add rsi, 4
-            add rdi, 4
-
-            mov eax, dword [rsi]
-            cmp eax, dword [rdi]
+            mov rax, qword [rsi + 16]
+            sub rax, qword [rdi + 16]
             jne .end
             
-            add rsi, 4
-            add rdi, 4
-
-            mov eax, dword [rsi]
-            cmp eax, dword [rdi]
-            jne .end
-
-            add rsi, 4
-            add rdi, 4
-
-            mov eax, dword [rsi]
-            cmp eax, dword [rdi]
-            jne .end
-
-            add rsi, 4
-            add rdi, 4
-
-            mov eax, dword [rsi]
-            cmp eax, dword [rdi]
-            jne .end
-
-            add rsi, 4
-            add rdi, 4
-
-            mov eax, dword [rsi]
-            cmp eax, dword [rdi]
-            jne .end
-            
-            add rsi, 4
-            add rdi, 4
-
-            mov eax, dword [rsi]
-            cmp eax, dword [rdi]
-            jne .end
+            mov rax, qword [rsi + 24]
+            sub rax, qword [rdi + 24]
 
 .end:
-            mov ebx, dword [rdi]
-            
-            sub eax, ebx 
-            ; push qword [ret_addr]
 
             ret 
 ;=============================================
 ;=============================================
-
-
-section .data
-
-ret_addr        dq 0
-printing_str    db "hello %s %s", 0
-rsi_            dq 0
-rdi_            dq 0
-rax_            dq 0
-rcx_            dq 0
-msg             db "%s %s", 0
